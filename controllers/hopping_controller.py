aa2dbb5c112ba86d4334f33ae6b71cbb9bf1366a
Aleksandr Rodin <alex@Saule.local>
2025-04-29 15:15:16 +0800

    Added ths hopping segments. Working out their shifts now for easier visualization

--- controllers/hopping_controller.py
+++ controllers/hopping_controller.py
@@ -66,7 +66,7 @@ class HoppingController(QObject):
         # Internal controller state
         self.state_info = (
             []
-        )  # Tuples of (site_name, state_name, state_id) for the states in the selected unit cell
+        )  # Tuples of (site_name, site_id, state_name, state_id) for the states in the selected unit cell
         self.pair_selection = [
             None,
             None,
@@ -164,7 +164,7 @@ class HoppingController(QObject):
                 # Show the state and site names.
                 # From second quantization, the hopping goes FROM column INTO row
                 # (columns multiply annihilation operators, rows multiply creation)
-                btn.setToolTip(f"{state2[0]}.{state2[1]} → {state1[0]}.{state1[1]}")
+                btn.setToolTip(f"{state2[0]}.{state2[2]} → {state1[0]}.{state1[2]}")
 
                 # Button click handler implementation:
                 # 1. Qt buttons can only connect to argumentless functions
@@ -220,8 +220,8 @@ class HoppingController(QObject):
         # Iterate through all buttons and update their colors
         for pos, btn in self.buttons.items():
             ii, jj = pos
-            s1 = self.state_info[ii][2]  # Destination state ID
-            s2 = self.state_info[jj][2]  # Source state ID
+            s1 = self.state_info[ii][3]  # Destination state ID
+            s2 = self.state_info[jj][3]  # Source state ID
             hop = set(self.hopping_data.get((s1, s2), []))
             hop_herm = set(self.hopping_data.get((s2, s1), []))
             has_hopping = bool(hop)
@@ -239,19 +239,19 @@ class HoppingController(QObject):
         Updates the table to display hopping terms between the selected states.
 
         Args:
-            s1: Tuple of (site_name, state_name, state_id) for the destination state (row)
-            s2: Tuple of (site_name, state_name, state_id) for the source state (column)
+            s1: Tuple of (site_name, site_id, state_name, state_id) for the destination state (row)
+            s2: Tuple of (site_name, site_id, state_name, state_id) for the source state (column)
         """
         # Store the UUIDs of the selected states
         self.pair_selection = [s1, s2]
         self.hopping_view.table_stack.setCurrentWidget(self.hopping_view.table_panel)
 
         # Retrieve existing hopping terms between these states, or empty list if none exist
-        self.state_coupling = self.hopping_data.get((s1[2], s2[2]), [])
+        self.state_coupling = self.hopping_data.get((s1[3], s2[3]), [])
 
         # Update the table title to show the selected states (source → destination)
         self.hopping_view.table_panel.table_title.setText(
-            f"{s2[0]}.{s2[1]} → {s1[0]}.{s1[1]}"
+            f"{s2[0]}.{s2[2]} → {s1[0]}.{s1[2]}"
         )
         self._refresh_table()
 
@@ -384,7 +384,7 @@ class HoppingController(QObject):
             for (d1, d2, d3), amplitude in new_couplings.items()
         ]
         # Update the data model with the new couplings
-        self.hopping_data[(self.pair_selection[0][2], self.pair_selection[1][2])] = (
+        self.hopping_data[(self.pair_selection[0][3], self.pair_selection[1][3])] = (
             merged_couplings
         )
         self.unit_cells[self.selection["unit_cell"]].hoppings = self.hopping_data
@@ -420,23 +420,23 @@ class HoppingController(QObject):
         menu.exec_(button.mapToGlobal(QPoint(0, button.height())))
 
     def _create_hermitian_partner(self, ii, jj):
-        s1 = self.state_info[ii][2]  # Destination
-        s2 = self.state_info[jj][2]  # Source
+        s1 = self.state_info[ii][3]  # Destination
+        s2 = self.state_info[jj][3]  # Source
         hop = self.hopping_data.get((s1, s2), [])
         hop_herm = [((-d1, -d2, -d3), np.conj(x)) for ((d1, d2, d3), x) in hop]
         self.hopping_data[(s2, s1)] = hop_herm
         self.hoppings_changed.emit()
 
     def _delete_coupling(self, ii, jj):
-        s1 = self.state_info[ii][2]  # Destination
-        s2 = self.state_info[jj][2]  # Source
+        s1 = self.state_info[ii][3]  # Destination
+        s2 = self.state_info[jj][3]  # Source
         self.hopping_data.pop((s1, s2), None)
         self.hoppings_changed.emit()
 
     def _handle_matrix_interaction(self):
         self._refresh_button_colors()
         updated_couplings = self.hopping_data.get(
-            (self.pair_selection[0][2], self.pair_selection[1][2]), []
+            (self.pair_selection[0][3], self.pair_selection[1][3]), []
         )
         self.state_coupling = updated_couplings
 
--- controllers/uc_plot_controller.py
+++ controllers/uc_plot_controller.py
@@ -52,8 +52,6 @@ class UnitCellPlotController(QObject):
         self.unit_cell = None
         self.uc_plot_items = {}  # Dictionary to store plot items
 
-        self.uc_hopping_segments = {}  # Dictionary to store hopping segments
-
     def update_unit_cell(self, wireframe_shown, n1, n2, n3):
         """
         Set or update the unit cell to be displayed in the 3D view.
@@ -216,36 +214,52 @@ class UnitCellPlotController(QObject):
     def update_hopping_segments(self, pair_selection):
 
         # Clear previous hopping segments
-        for key, item in list(self.uc_hopping_segments.items()):
-            self.uc_plot_view.view.removeItem(item)
-            del self.uc_plot_items[key]
+
+        hopping_segments = self.uc_plot_items.get("hopping_segments")
+        if hopping_segments is not None:
+            self.uc_plot_view.view.removeItem(hopping_segments)
+            del self.uc_plot_items["hopping_segments"]
 
         # s1 and s2 are tuples (site_name, state_name, state_id)
         s1, s2 = pair_selection
-        hoppings = self.unit_cell.hoppings.get((s1[2], s2[2]))
-        print(s1)
-        print(s2)
-        if hoppings is not None:
-            ds = [x[0] for x in hoppings]
-        else:
-            ds = []
+        hoppings = self.unit_cell.hoppings.get((s1[3], s2[3]))
+        if hoppings is None:
+            return
+
+        v1 = self.unit_cell.v1.as_array()
+        v2 = self.unit_cell.v2.as_array()
+        v3 = self.unit_cell.v3.as_array()
 
         # Get the location of the source site
         source = self.unit_cell.sites[s2[1]]
-        source_pos = (
-            source.c1 * self.unit_cell.v1.as_array()
-            + source.c2 * self.unit_cell.v2.as_array()
-            + source.c3 * self.unit_cell.v3.as_array()
-        )
-        # Get the locations of the target sites
+        source_pos = source.c1 * v1 + source.c2 * v2 + source.c3 * v3
+
+        # Get the location of the target sites in the (0,0,0) unit cell
         target = self.unit_cell.sites[s1[1]]
         target_pos = (
             target.c1 * self.unit_cell.v1.as_array()
             + target.c2 * self.unit_cell.v2.as_array()
             + target.c3 * self.unit_cell.v3.as_array()
         )
-        # print(ds)
-        print(source_pos)
-        print(target_pos)
-        # s1, s2 = pair_selection
-        # print(s1, s2)
+
+        segments = []
+        for (d1, d2, d3), _ in hoppings:
+            target = target_pos + d1 * v1 + d2 * v2 + d3 * v3
+            segments.append(source_pos)
+            segments.append(target)
+        segment_array = np.array(segments, dtype=np.float32)
+
+        hopping_segments = gl.GLLinePlotItem(
+            pos=segment_array,
+            color=(1.0, 0.0, 0.0, 1.0),  # Red color (RGBA)
+            width=5,
+            mode="lines",
+        )
+
+        shift = -(self.n1 * v1 + self.n2 * v2 + self.n3 * v3) / 2
+        hopping_segments.translate(shift[0], shift[1], shift[2])
+
+        # Add to the view
+        self.uc_plot_view.view.addItem(hopping_segments)
+        # # Store it so we can remove it later
+        self.uc_plot_items["hopping_segments"] = hopping_segments
