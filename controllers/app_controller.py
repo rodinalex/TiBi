fec9f4b1b68fc691aa36d7d7b8f96ba5dd006c94
rodinalex <alerodin@gmail.com>
2025-05-19 15:17:45 +0800

    Added documentation and cleaned up app_controller

--- controllers/app_controller.py
+++ controllers/app_controller.py
@@ -1,139 +1,159 @@
-from PySide6.QtCore import QObject
-import numpy as np
 import copy
+import numpy as np
+from PySide6.QtCore import QObject
+import uuid
+
+from controllers.bz_plot_controller import BrillouinZonePlotController
+from controllers.computation_controller import ComputationController
+from controllers.hopping_controller import HoppingController
+from controllers.main_ui_controller import MainUIController
+from controllers.uc_controller import UnitCellController
+from controllers.uc_plot_controller import UnitCellPlotController
+from src.tibitypes import UnitCell
 
 
 class AppController(QObject):
     """
-    Main application controller that orchestrates interactions between components.
+    Main application controller.
 
-    This controller serves as a high-level coordinator in the application, handling
-    communication between different subsystems. It connects signals from various
-    controllers and routes requests to appropriate handlers, ensuring that components
-    remain decoupled from each other.
+    This controller serves as a high-level coordinator in the application,
+    handling communication between different subsystems.
+    It connects signals from various controllers and routes requests to
+    appropriate handlers, ensuring that components remain decoupled from
+    each other.
+
+    Attributes
+    ----------
+    models : dict
+        Dictionary of data models used in the application
+    controllers : dict
+        Dictionary of controllers for different application components
     """
 
     def __init__(self, models, controllers):
         """
         Initialize the application controller.
 
-        Args:
-            models: Dictionary of data models used in the application
-            controllers: Dictionary of controllers for different application components
+        Parameters
+        ----------
+        models : dict
+            Dictionary of data models used in the application
+        controllers : dict
+            Dctionary of controllers for different application components
         """
         super().__init__()
         self.models = models
         self.controllers = controllers
 
+        # Extract the relevant models and controllers
+        self.bz_plot_controller: BrillouinZonePlotController = (
+            self.controllers["bz_plot"]
+        )
+        self.computation_controller: ComputationController = self.controllers[
+            "computation"
+        ]
+        self.hopping_controller: HoppingController = self.controllers[
+            "hopping"
+        ]
+        self.main_ui_controller: MainUIController = self.controllers["main_ui"]
+        self.uc_controller: UnitCellController = self.controllers["uc"]
+        self.uc_plot_controller: UnitCellPlotController = self.controllers[
+            "uc_plot"
+        ]
+
+        self.selection: dict[str, uuid.UUID] = self.models["selection"]
+        self.unit_cells: dict[uuid.UUID, UnitCell] = self.models["unit_cells"]
+
         # Connect signals
 
         # When an item in the tree view is renamed, refresh the hopping matrix
         # and table to reflect the correct names
-        self.controllers["uc"].item_changed.connect(self._handle_item_changed)
+        self.uc_controller.item_changed.connect(self._handle_item_changed)
 
         # Handle the programmatic selection of an item in the tree
         # due to undo/redo in the hopping controller
-        self.controllers["hopping"].selection_requested.connect(
+        self.hopping_controller.selection_requested.connect(
             self._handle_selection_requested
         )
-        ############
 
-        self.controllers["uc"].plot_update_requested.connect(
+        # Refresh the plots after unit cell selection or parameter change
+        self.uc_controller.plot_update_requested.connect(
             self._handle_plot_update_requested
         )
 
-        self.controllers["hopping"].hopping_segments_requested.connect(
+        # Handle the request to draw hopping segments after a pair of states
+        # is selected from the hopping button matrix
+        self.hopping_controller.hopping_segments_requested.connect(
             self._handle_hopping_segments_requested
         )
 
-        self.controllers["computation"].status_updated.connect(
-            self._relay_status
-        )
+        self.computation_controller.status_updated.connect(self._relay_status)
 
         # Toolbar signals
 
-        self.controllers["main_ui"].project_refresh_requested.connect(
+        self.main_ui_controller.project_refresh_requested.connect(
             self._handle_project_refresh_requested
         )
 
-        self.controllers["main_ui"].toolbar.n1_spinbox.valueChanged.connect(
+        # Update the plots when the number of unit cells to be plotted changes
+        self.main_ui_controller.toolbar.n1_spinbox.valueChanged.connect(
             self._handle_plot_update_requested
         )
-        self.controllers["main_ui"].toolbar.n2_spinbox.valueChanged.connect(
+        self.main_ui_controller.toolbar.n2_spinbox.valueChanged.connect(
             self._handle_plot_update_requested
         )
-        self.controllers["main_ui"].toolbar.n3_spinbox.valueChanged.connect(
+        self.main_ui_controller.toolbar.n3_spinbox.valueChanged.connect(
             self._handle_plot_update_requested
         )
 
-        # Action signals
-        self.controllers["main_ui"].wireframe_toggled.connect(
+        # Toggle the wireframe in the unit cell plot
+        self.main_ui_controller.wireframe_toggled.connect(
             self._handle_wireframe_toggled
         )
 
     def _relay_status(self, msg):
         """
-        Relaying function that sends status updates from various controllers to the main_ui
-        controller to be displayed in the status bar.
+        Send a message to the status bar.
         """
-        self.controllers["main_ui"].update_status(msg)
+        self.main_ui_controller.update_status(msg)
 
     def _handle_plot_update_requested(self):
         """
-        Handle requests to update unit cell and Brollouin zone plots.
-
-        This method is triggered when either a new unit cell is selected or when
-        a currently-selected unit cell is modified. Because the plotting controllers
-        have access to the unit_cells dictionary and the selection, no information needs
-        to be passed.
+        Handle requests to update unit cell and Brillouin zone plots.
         """
-
-        # Depending on the dimensionality, update the unit cell spinners in the toolbar
-
-        uc_id = self.models["selection"].get("unit_cell")
-        # print("UCID")
-        # print(uc_id)
+        uc_id = self.selection.get("unit_cell")
         if uc_id is not None:
-            unit_cell = self.models["unit_cells"][uc_id]
+            unit_cell = self.unit_cells[uc_id]
 
-            # Check which vectors of the unit cell are periodic and activate the UC spinners if they are
-            if unit_cell.v1.is_periodic == True:
-                self.controllers["main_ui"].toolbar.n1_spinbox.setEnabled(True)
-            else:
-                self.controllers["main_ui"].toolbar.n1_spinbox.setEnabled(
-                    False
-                )
+            # Check which vectors of the unit cell are periodic and activate
+            # the UC spinners if they are
+            self.main_ui_controller.toolbar.n1_spinbox.setEnabled(
+                unit_cell.v1.is_periodic
+            )
+            self.main_ui_controller.toolbar.n2_spinbox.setEnabled(
+                unit_cell.v2.is_periodic
+            )
+            self.main_ui_controller.toolbar.n3_spinbox.setEnabled(
+                unit_cell.v3.is_periodic
+            )
 
-            if unit_cell.v2.is_periodic == True:
-                self.controllers["main_ui"].toolbar.n2_spinbox.setEnabled(True)
-            else:
-                self.controllers["main_ui"].toolbar.n2_spinbox.setEnabled(
-                    False
-                )
-
-            if unit_cell.v3.is_periodic == True:
-                self.controllers["main_ui"].toolbar.n3_spinbox.setEnabled(True)
-            else:
-                self.controllers["main_ui"].toolbar.n3_spinbox.setEnabled(
-                    False
-                )
         else:
-            self.controllers["main_ui"].toolbar.n1_spinbox.setEnabled(False)
-            self.controllers["main_ui"].toolbar.n2_spinbox.setEnabled(False)
-            self.controllers["main_ui"].toolbar.n3_spinbox.setEnabled(False)
+            self.main_ui_controller.toolbar.n1_spinbox.setEnabled(False)
+            self.main_ui_controller.toolbar.n2_spinbox.setEnabled(False)
+            self.main_ui_controller.toolbar.n3_spinbox.setEnabled(False)
 
         n1, n2, n3 = [
             spinbox.value() if spinbox.isEnabled() else 1
             for spinbox in (
-                self.controllers["main_ui"].toolbar.n1_spinbox,
-                self.controllers["main_ui"].toolbar.n2_spinbox,
-                self.controllers["main_ui"].toolbar.n3_spinbox,
+                self.main_ui_controller.toolbar.n1_spinbox,
+                self.main_ui_controller.toolbar.n2_spinbox,
+                self.main_ui_controller.toolbar.n3_spinbox,
             )
         ]
         wireframe_shown = (
-            self.controllers["main_ui"]
-            .action_manager.unit_cell_actions["wireframe"]
-            .isChecked()
+            self.main_ui_controller.action_manager.unit_cell_actions[
+                "wireframe"
+            ].isChecked()
         )
         band_structure = self.models["band_structures"].get(uc_id)
         self.models["bz_path"].clear()
@@ -159,40 +179,44 @@ class AppController(QObject):
                 {"k_path": None, "bands": None, "special_points": None}
             )
 
-        self.controllers["uc_plot"].update_unit_cell(
-            wireframe_shown, n1, n2, n3
-        )
-        self.controllers["bz_plot"].update_brillouin_zone()
+        self.uc_plot_controller.update_unit_cell(wireframe_shown, n1, n2, n3)
+        self.bz_plot_controller.update_brillouin_zone()
 
     def _handle_wireframe_toggled(self, status):
         """
         Handle the toggling of the wireframe button in the toolbar.
 
-        Extract the number of unit cells to be plotted along each direction from the
-        corresponding spinboxes, check whether the wireframe is toggled on or off,
-        and call the update_unit_cell function with the relevant parameters.
+        Extract the number of unit cells to be plotted along each direction
+        from the corresponding spinboxes, check whether the wireframe
+        is toggled on or off, and call the `update_unit_cell`
+        function with the relevant parameters.
+
+        Parameters
+        ----------
+        status : bool
+            The status of the wireframe toggle button.
         """
         n1, n2, n3 = [
             spinbox.value() if spinbox.isEnabled() else 1
             for spinbox in (
-                self.controllers["main_ui"].toolbar.n1_spinbox,
-                self.controllers["main_ui"].toolbar.n2_spinbox,
-                self.controllers["main_ui"].toolbar.n3_spinbox,
+                self.main_ui_controller.toolbar.n1_spinbox,
+                self.main_ui_controller.toolbar.n2_spinbox,
+                self.main_ui_controller.toolbar.n3_spinbox,
             )
         ]
-        self.controllers["uc_plot"].update_unit_cell(status, n1, n2, n3)
+        self.uc_plot_controller.update_unit_cell(status, n1, n2, n3)
 
     def _handle_hopping_segments_requested(self):
         """
-        Handle the request to draw hopping segments after a pair of states
-        is selected from the hopping button matrix.
+        Handle the request to draw hopping segments.
 
-        The function obtains the states selected by the button click and passes
-        them to the update_hopping_segments function to draw the lines
-        connecting the source state with the destination ones.
+        After a pair of states is selected from the hopping button matrix,
+        this function passes them to the update_hopping_segments function
+        to draw the lines connecting the source state with the destination
+        ones.
         """
-        pair_selection = self.controllers["hopping"].pair_selection
-        self.controllers["uc_plot"].update_hopping_segments(pair_selection)
+        pair_selection = self.hopping_controller.pair_selection
+        self.uc_plot_controller.update_hopping_segments(pair_selection)
 
     def _handle_item_changed(self):
         """
@@ -200,18 +224,18 @@ class AppController(QObject):
 
         This function is necessary to make sure that the label names in the
         hopping matrix and hopping table accurately reflect the item names.
+        The effect is the redrawing of the hopping matrix.
         """
-        self.controllers["hopping"].update_unit_cell()
+        self.hopping_controller.update_unit_cell()
 
     def _handle_project_refresh_requested(self):
         """
-        Handle a project refresh request. The unit cell dictionary and the project path have already been updated.
+        Handle a project refresh request. The unit cell dictionary and
+        the project path have already been updated.
         Here, the models and the views are refreshed.
         """
         # Current selection state (tracks which items are selected in the UI)
-        self.models["selection"].update(
-            {"unit_cell": None, "site": None, "state": None}
-        )
+        self.selection.update({"unit_cell": None, "site": None, "state": None})
 
         # Form data for the currently selected unit cell
         self.models["unit_cell_data"].update(
@@ -243,12 +267,11 @@ class AppController(QObject):
             {"k_path": None, "bands": None, "special_points": None}
         )
 
-        self.controllers["uc"].tree_view.refresh_tree(
-            self.models["unit_cells"]
-        )
+        self.uc_controller.tree_view.refresh_tree(self.models["unit_cells"])
         self._handle_plot_update_requested()
 
     def _handle_selection_requested(self, uc_id, site_id, state_id):
-        self.controllers["uc"].tree_view._select_item_by_id(
+        """Programmatically select an item in the tree view."""
+        self.uc_controller.tree_view._select_item_by_id(
             uc_id, site_id, state_id
         )
